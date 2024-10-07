487c8198f00d00c390c49a5d90d79cc31ed4ffd7
Carlos Toscano <toscanotoro10@gmail.com>
2024-10-06 18:26:37 -0600

    propuestas Funcional

---
+++ src/app/propuesta/page.tsx
@@ -0,0 +1,16 @@
+
+import NabVar from "@/components/NabVar";
+import CrearPropuesta from "@/components/CrearPropuesta";
+import Footer from "@/components/Footer";
+
+const App = () =>{
+  return (
+    <main className="w-[90%] m-auto">
+      <NabVar />
+    <CrearPropuesta />
+
+      <Footer />
+    </main>
+  );
+}
+export default App;
