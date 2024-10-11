776728906c49ab09f6612413721e806849a399c2
Carlos Toscano <toscanotoro10@gmail.com>
2024-10-11 16:06:54 -0600

    Add Admin file

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,13 @@
+
+import NabVar from '@/components/NabVar';
+import React from 'react';
+
+const AdminPage = () => {
+  return (
+    <div>
+        <NabVar />
+    </div>
+  );
+}
+
+export default AdminPage;
